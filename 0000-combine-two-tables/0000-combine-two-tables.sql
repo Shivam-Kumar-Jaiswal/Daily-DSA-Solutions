d210cf958a50b868337397312ac96928e8f8b324
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-05-24 00:58:45 +0530

    Time: 395 ms (76.54%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0000-combine-two-tables/0000-combine-two-tables.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on p.personId=a.personId
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
