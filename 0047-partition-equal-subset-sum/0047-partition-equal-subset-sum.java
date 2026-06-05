90708bdd2dd64ed2b7d7e668d644496ad4f1eb3f
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-06-05 16:03:08 +0530

    Time: 49 ms (50.22%), Space: 47.5 MB (53.79%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0047-partition-equal-subset-sum/0047-partition-equal-subset-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0,target=0,n=nums.length;
+        for( int e:nums){
+            sum+=e;
+        }target=sum/2;
+        if(sum%2==1){
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target+1];   
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target)
+        dp[0][nums[0]] =true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                boolean notTake = dp[i-1][j];
+                boolean take=false;
+                if(j>=nums[i])
+                take = dp[i-1][j-nums[i]];
+                dp[i][j]=take||notTake;
+            }
+        }
+        return dp[n-1][target];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
