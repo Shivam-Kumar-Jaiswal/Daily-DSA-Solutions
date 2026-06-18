61892a42b2e8aa61d0d1e2ef7be8583aee915618
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-06-18 15:45:06 +0530

    Time: 3 ms (98.41%), Space: 47 MB (70.31%) - LeetHub - Time: O(m*n), Space: O(m*n)

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minDistance(String word1, String word2){
+        
+        int[][] dp = new int[word1.length()][word2.length()];
+        for(int[] e:dp)
+        Arrays.fill(e,-1);
+        return helper(word1,word2,0,0,word1.length(),word2.length(),dp);
+    }
+    int helper(String w1,String w2,int i,int j,int l1,int l2,int[][] dp){
+           
+          if(j==l2&&i==l1)
+          return 0;
+          if(j==l2){
+            return l1-i;
+          }
+          if(i==l1){
+            return l2-j;
+          }
+           if(dp[i][j]!=-1)
+            return dp[i][j];
+        int m=Integer.MAX_VALUE;
+        if(w1.charAt(i)==w2.charAt(j))
+         {dp[i][j]= helper(w1,w2,i+1,j+1,l1,l2,dp);
+         return dp[i][j];
+         }
+
+        
+        else{int re = 1+helper(w1,w2,i+1,j+1,l1,l2,dp);
+        int insert = 1+ helper(w1,w2,i,j+1,l1,l2,dp);
+        int  del = 1+ helper(w1,w2,i+1,j,l1,l2,dp);
+        dp[i][j] =  Math.min(re,Math.min(insert,del));
+        return dp[i][j];
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
