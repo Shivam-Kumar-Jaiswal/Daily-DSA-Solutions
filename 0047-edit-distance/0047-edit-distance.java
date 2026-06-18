9070ffbabeb30b9e29b3630ad23d456f56b62bd4
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-06-18 15:42:08 +0530

    Time: 8 ms (3.42%), Space: 47.5 MB (5.97%) - LeetHub - Time: O(m*n), Space: O(m*n)

--- 0047-edit-distance/0047-edit-distance.java
+++ 0047-edit-distance/0047-edit-distance.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minDistance(String word1, String word2){
-        if((word1.equals("teacher"))&&(word2.equals("tenace")))
-        return 3;
+        
         int[][] dp = new int[word1.length()][word2.length()];
         for(int[] e:dp)
         Arrays.fill(e,-1);
@@ -24,12 +23,8 @@ class Solution {
          m = helper(w1,w2,i+1,j+1,l1,l2,dp);
 
         int re = 1+helper(w1,w2,i+1,j+1,l1,l2,dp);
-        int insert =Integer.MAX_VALUE;
-        if((l1-i)<=(l2-j)) 
-        insert = 1+ helper(w1,w2,i,j+1,l1,l2,dp);
-        int del =Integer.MAX_VALUE;
-        if((l1-i)>=(l2-j)) 
-         del = 1+ helper(w1,w2,i+1,j,l1,l2,dp);
+        int insert = 1+ helper(w1,w2,i,j+1,l1,l2,dp);
+        int  del = 1+ helper(w1,w2,i+1,j,l1,l2,dp);
         dp[i][j] =  Math.min(m,Math.min(re,Math.min(insert,del)));
         return dp[i][j];
     }
