448d63213710382bdb67bc8d0dc9b0dabb04d7f1
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-06-23 00:55:16 +0530

    Time: 1 ms (78.55%), Space: 42.7 MB (21.88%) - LeetHub - Time: O(log n), Space: O(log n)

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isHappy(int n) {
+       //1st way 
+      /* int llow=n,fast=n;
+       do{
+          llow=sum(llow);
+          fast=sum(sum(fast));
+       }while(fast!=llow);
+       if(llow==1)
+            return true;
+       return false;
+    }
+     int sum(int n){
+        r=0;
+        while(n>0){
+            r+=((n%10)*(n%10));
+            n/=10;
+        }
+        return r;
+    */
+    HashMap<Integer,Boolean> h = new HashMap<>();
+    h.put(n,true);
+    while(true){
+        int r=0;
+        while(n>0){
+            r=r+(n%10)*(n%10);
+            n/=10;
+        }
+        if(r==1)
+        return true;
+        if(h.containsKey(r))
+        return false;
+        h.put(r,true);
+        n=r;
+
+    }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
