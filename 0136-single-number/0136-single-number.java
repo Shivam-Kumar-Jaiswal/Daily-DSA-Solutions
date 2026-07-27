7a19df65a0c280950bc8983b253761b86e06b388
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-07-27 15:14:15 +0530

    Time: 1 ms (99.96%), Space: 47 MB (46.32%) -

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int e: nums){
+            result^=e;
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
