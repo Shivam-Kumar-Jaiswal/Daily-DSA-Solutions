5ed551f473e2d9234605de789443b874b6352555
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-08-24 13:29:30 +0530

    Time: 4 ms (5.1%), Space: 72.4 MB (68.65%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int[] arr = {0,0,0};
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                arr[0]++;
+            }
+            if(bills[i]==10){
+                if(arr[0]==0)return false;
+                arr[0]--;
+                arr[1]++;
+            }
+            if(bills[i]==20){
+                if(arr[1]>0){
+                    arr[1]--;
+                    if(arr[0]>0)
+                    arr[0]--;
+                    else return false;
+                }
+                else{
+                    if(arr[0]>=3){
+                        arr[0]-=3;
+                    }
+                    else return false;
+                }
+                arr[2]++;
+            }
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
