a6c7f78b5a00bd68d8ae8f068b14132c7d99926e
Shivam-Kumar-Jaiswal <147632532+Shivam-Kumar-Jaiswal@users.noreply.github.com>
2026-09-15 14:05:33 +0530

    Added solution - Time: O(n), Space: O(1)

--- 703089-second-largest-GFG/703089-second-largest.java
+++ 703089-second-largest-GFG/703089-second-largest.java
@@ -1,20 +1,20 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        int n=arr.length;
-        int max1=-1;
-        int max2=-1;
-        for(int i=0;i<n;i++){
-            if(max1<arr[i]){
-                max2=max1;
-                max1=arr[i];
+        int lar=Integer.MIN_VALUE,sl=Integer.MIN_VALUE,i;
+        for(i=0;i<arr.length;i++){
+            if(arr[i]>lar){
+                sl=lar;
+                lar=arr[i];
             }
-            else if(max2<arr[i] && arr[i]<max1){
-                
-                max2=arr[i];
+            else if(arr[i]>sl){
+                if(arr[i]!=lar)
+                sl = arr[i];
             }
         }
-        return max2;
+        if(sl==Integer.MIN_VALUE)
+        return -1;
+        return sl;
     }
 }
 
